98b93e9858a6950f79373a1496b67f1334b5229c
Leandro Lima <leo.lima@me.com>
2024-11-27 19:01:25 -0300

    Traduzindo Subscritions de Português para Inglês

--- frontend/src/pages/home/components/Subscription.tsx
+++ frontend/src/pages/home/components/Subscription.tsx
@@ -6,43 +6,38 @@ const Subscription: React.FC = () => {
   return (
     <div className="subscription-container">
       <section className="intro">
-        <h1>Assine nossos planos e ganhe benefícios exclusivos!</h1>
+        <h1>Subscribe to our plans and enjoy exclusive benefits!</h1>
         <p>
-          Oferecemos programas de fidelidade para nossos clientes. Assine agora
-          e aproveite descontos, promoções especiais e muito mais!
+          We offer loyalty programs for our customers. Subscribe now and take
+          advantage of discounts, special promotions, and much more!
         </p>
       </section>
 
       <section className="plans">
-        <h2>Escolha o seu plano de fidelidade</h2>
+        <h2>Choose Your Loyalty Plan</h2>
         <div className="plan">
           <div className="plan-info">
-            <h3>Plano Básico</h3>
-            <p>
-              Benefícios: Descontos em serviços, acesso a promoções semanais.
-            </p>
+            <h3>Basic Plan</h3>
+            <p>Benefits: Discounts on services, access to weekly promotions.</p>
           </div>
           <div className="plan-image">
-            <img src={loyaltyCardImage} alt="Plano Básico" />
+            <img src={loyaltyCardImage} alt="Basic Plan" />
           </div>
         </div>
         <div className="plan">
           <div className="plan-info">
-            <h3>Plano Premium</h3>
-            <p>
-              Benefícios: Descontos exclusivos, agendamentos prioritários e
-              mais!
-            </p>
+            <h3>Premium Plan</h3>
+            <p>Benefits: Exclusive discounts, priority scheduling, and more!</p>
           </div>
           <div className="plan-image">
-            <img src={loyaltyCardImage} alt="Plano Premium" />
+            <img src={loyaltyCardImage} alt="Premium Plan" />
           </div>
         </div>
       </section>
 
       <section className="call-to-action">
-        <h2>Inscreva-se agora e comece a economizar!</h2>
-        <button className="signup-btn">Assine Agora</button>
+        <h2>Sign up now and start saving!</h2>
+        <button className="signup-btn">Subscribe Now</button>
       </section>
     </div>
   );
