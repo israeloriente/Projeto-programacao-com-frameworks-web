c9e90f212eded66dc765c75ade1164ac3ba64d3b
israeloriente <israelnunesoriente@gmail.com>
2024-11-27 14:37:56 +0000

    feat: Show guards message

--- frontend/src/guards/ProtectedRoute.tsx
+++ frontend/src/guards/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthService } from "../services/auth.service";
+import { Alert } from "../components/global/Alert";
 
 interface ProtectedRouteProps {
   children: ReactNode;
@@ -8,6 +9,7 @@ interface ProtectedRouteProps {
 
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   if (!AuthService.isAuthenticated()) {
+    Alert.showToast("You do not have permission to access this page.", "error");
     return <Navigate to="/login" />;
   }
 
